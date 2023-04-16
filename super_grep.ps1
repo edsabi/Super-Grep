981ffbd8c368018ea7bf79489f42e34aa5d4ece1
edsabi <38739707+edsabi@users.noreply.github.com>
2023-04-16 08:46:42 -0400

    Update super_grep.ps1

--- super_grep.ps1
+++ super_grep.ps1
@@ -2,16 +2,47 @@
 param (
     [Parameter()]
     [string]$Path = "",
-    [Parameter(Mandatory=$true)]
+    [Parameter()]
     [string]$String,
-    [int]$SleepDurationInSeconds = 2
+    [Parameter()]
+    [string]$OutputFile,
+    [int]$SleepDurationInSeconds = 2,
+    [Parameter()]
+    [switch]$Help
 )
+
+function Show-Help {
+    @"
+Usage:
+    .\code_scraper.ps1 [-Path <Path>] -String <String> [-OutputFile <OutputFile>] [-SleepDurationInSeconds <SleepDurationInSeconds>] [-Help]
+
+Options:
+    -Path <Path>                      : The path to search for files (default is the current path).
+    -String <String>                  : The string to search for in the files (mandatory).
+    -OutputFile <OutputFile>          : The output file to save the results (optional, results will be printed to console if not provided).
+    -SleepDurationInSeconds <Seconds> : The number of seconds to sleep between code blocks (default is 2).
+    -Help                             : Show this help message.
+
+Examples:
+    .\code_scraper.ps1 -String "svg"
+    .\code_scraper.ps1 -Path "C:\path\to\your\folder" -String "svg" -OutputFile "output.txt" -SleepDurationInSeconds 3
+"@
+}
+
+if ($Help -or ($String -eq "")) {
+    if ($String -eq "") {
+        Write-Host "Error: The -String parameter is mandatory.`n" -ForegroundColor Red
+    }
+    Show-Help
+    exit
+}
 
 if ($Path -eq "") {
     $Path = (Resolve-Path .).Path
 }
 
 $alreadyPrintedCodeBlocks = @{}
+$outputData = @()
 
 Get-ChildItem -Path $Path -Recurse -File | ForEach-Object {
     $filePath = $_.FullName
@@ -35,24 +66,23 @@ Get-ChildItem -Path $Path -Recurse -File | ForEach-Object {
             $codeBlockString = $codeBlock -join "`n"
 
             if (-not $alreadyPrintedCodeBlocks.ContainsKey($codeBlockString)) {
-                Write-Host "Found '$String' in file: $filePath"
-                Write-Host "Line number: $($i + 1)"
-                
-                try {
-                    Write-Host "Code block:"
-                    $codeBlock | ForEach-Object { Write-Host "$_" }
-                    Write-Host ""
+                $output = "Found '$String' in file: $filePath`nLine number: $($i + 1)`nCode block:`n$codeBlockString`n"
+                $outputData += $output
 
+                if (-not $OutputFile) {
+                    Write-Host $output
+                    
                     # Sleep for the specified duration
                     Start-Sleep -Seconds $SleepDurationInSeconds
+                }
 
-                    # Mark the code block as printed
-                    $alreadyPrintedCodeBlocks[$codeBlockString] = $true
-                }
-                catch {
-                    Write-Host "Error printing code block at line $($i + 1) in file: $filePath`n"
-                }
+                # Mark the code block as printed
+                $alreadyPrintedCodeBlocks[$codeBlockString] = $true
             }
         }
     }
+}
+
+if ($OutputFile) {
+    $outputData | Set-Content -Path $OutputFile
 }
